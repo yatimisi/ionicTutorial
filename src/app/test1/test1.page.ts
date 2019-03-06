48269d415bfe739783609636ab53f663e578bfa8
Yatimisi <yatimisi2012@gmail.com>
2019-03-07 00:18:27 +0800

    update

--- src/app/test1/test1.page.ts
+++ src/app/test1/test1.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-test1',
@@ -6,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test1.page.scss'],
 })
 export class Test1Page implements OnInit {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  constructor() { }
+  private data;
+  constructor() {
+    this.data = 0;
+  }
 
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done' + this.data.length);
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.data.length == 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
   ngOnInit() {
   }
 
